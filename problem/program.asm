bbff61a4490fd858578c36eed7e52d7a072ddfd8
DANIEL HOYOS CORREA <dantethemaster034@gmail.com>
2022-02-09 00:01:50 -0500

    Asignacion de valores a espacios de memoria

--- problem/program.asm
+++ problem/program.asm
@@ -3,46 +3,18 @@
 // daniel.hoyos@upb.edu.co
 
 (BEGIN)
-	// Set keyboard to address for keyboard input value.
+	// Inicializamos la direccion de los input de teclado.
 	@24576
 	D=A
 	@keyboard
 	M=D
-(CHECK_KEYBOARD)
-	// Set current to last screen pixel map.
-	@24575
+	// Guardamos en memoria codigo de la tecla F.
+	@70
 	D=A
-	@current
+	@keycodef
 	M=D
-	// If keyboard is pressed, fill the screen.
-	@keyboard
-	A=M
-	D=M
-	@fillvalue
-	M=-1
-	@DRAW
-	D;JNE
-	// Otherwise, clear the screen.
-	@fillvalue
-	M=0
-(DRAW)
-	// Fill or clear current pixel, depending on fillvalue.
-	@fillvalue
-	D=M
-	@current
-	A=M
+	// Guardamos en memoria codigo de la tecla C.
+	@67
+	D=A
+	@keycodec
 	M=D
-	// If current pixel map is first pixel map there is nothing left to draw, so
-	//jump back to keyboard check.
-	@current
-	D=M
-	@16384
-	D=D-A
-	@CHECK_KEYBOARD
-	D;JLE
-	// Decrement current pixel map.
-	@current
-	M=M-1
-	// Continue drawing next pixel map.
-	@DRAW
-	0;JMP
