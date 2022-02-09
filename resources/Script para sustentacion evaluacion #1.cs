53546d60d0b89674a383b383011e5359f2d1dafb
DANIEL HOYOS CORREA <dantethemaster034@gmail.com>
2022-02-09 08:41:26 -0500

    Se agrego una carpeta de recursos

---
+++ resources/Script para sustentacion evaluacion #1.cs
@@ -0,0 +1,28 @@
+// Ciclo iterativo para asignar a cada espacio de memoria, correspondiente a cada pixel, la informacion de color.
+// En este caso negro (asignado por el numero -1).
+For(int i = 24575; i!=0; i--)
+{
+    pixel[i] = -1
+}
+
+// Ciclo iterativo para asignar a cada espacio de memoria, correspondiente a cada pixel, la informacion de color.
+// En este caso negro (asignado por el numero -1).
+For(int i = 24575; i!=0; i--)
+{
+    pixel[i] = 0
+}
+
+// Condicional que asigna el valor de llenado dependiendo de el input del teclado.
+// En caso de ser F, se asigna negro (-1), en caso de ser C, se asigna blanco (0), y en caso de ser otra no hacer nada.
+if(input.GetKey().keycode(F))
+{
+    valorDeLlenado = -1
+}
+elseif(input.GetKey().keycode(C))
+{
+    valorDeLlenado = 0;
+}
+else
+{
+    return;
+}
